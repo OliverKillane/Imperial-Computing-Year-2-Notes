d6ccd47b404ec9069fe2470ac7e8100bb303785c
Oliver Killane <oliver.killane20@imperial.ac.uk>
2021-11-07 23:56:24 +0000

    Models Lecture 1 Added

---
+++ 50003 - Models Of Computation/Lecture 1/code/euclid.hs
@@ -0,0 +1,6 @@
+-- Euclid's algorithm:
+-- continually take the modulus and compare until the modulus is zero
+euclidGCD :: Int -> Int -> Int
+euclidGCD a b
+  | b == 0 = a
+  | otherwise = euclidGCD b (a `mod` b)
