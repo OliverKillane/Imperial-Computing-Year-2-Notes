f0d379ba92ded037e2227c0eea38201d272072f1
Oliver Killane <oliver.killane20@imperial.ac.uk>
2021-11-27 23:48:13 +0000

    OS added Correction after confirming char_device_struct issue in slides with lecturer

---
+++ 50004 - Operating Systems/Lecture 12 - (Device Management)/code/char device struct.c
@@ -0,0 +1,17 @@
+static struct char_device_struct {
+	struct char_device_struct *next;
+	unsigned int major;
+	unsigned int baseminor;
+	int minorct;
+	char name[64];
+	struct cdev *cdev;		/* will die */
+} *chrdevs[CHRDEV_MAJOR_HASH_SIZE];
+
+struct cdev {
+	struct kobject kobj;
+	struct module *owner;
+	const struct file_operations *ops;
+	struct list_head list;
+	dev_t dev;
+	unsigned int count;
+} __randomize_layout;
